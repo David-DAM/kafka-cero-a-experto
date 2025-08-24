dc7bb30d7132048a3f1978c5433167d0ead0a1a8
David <david11jv@gmail.com>
2025-08-24 10:49:25 +0200

    Initial commit

---
+++ src/test/java/com/davinchicoder/kafka_cero_a_experto/KafkaCeroAExpertoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davinchicoder.kafka_cero_a_experto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaCeroAExpertoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
