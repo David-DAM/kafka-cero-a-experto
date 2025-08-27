b1cd9a484afca8f440b7e2ddfbfe7acbcde5adc7
David <david11jv@gmail.com>
2025-08-27 17:44:57 +0200

    Introduce Kafka infrastructure and user event handling
    - Added Kafka consumer and producer configurations (`KafkaConsumerConfig` and `KafkaProducerConfig`).
    - Created `UserKafkaEventConsumer` to handle user event messages from Kafka.
    - Introduced `ApplicationConfig` to enable Kafka in the application context.
    - Added AsyncAPI documentation (`asyncapi.yaml`) for user events.
    - Updated application properties to YAML format and added Kafka-related configurations.

---
+++ src/main/java/com/davinchicoder/kafka_cero_a_experto/common/infrastructure/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.davinchicoder.kafka_cero_a_experto.common.infrastructure;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class ApplicationConfig {
+}
